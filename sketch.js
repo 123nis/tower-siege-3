951f6f9fb5194117faf94dc18108023053c8f8c4
NISCHAY BOTHRA <73829368+123nis@users.noreply.github.com>
2020-12-17 09:45:40 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -76,10 +76,10 @@ function draw() {
   text(mouseX + ',' + mouseY, 10, 15);
   textSize(20);
   fill("lightyellow");
-  text("Drag the polygon to destroy the blocks",300,30);
+  text(" Destroy the blocks",300,30);
   text("SCORE : "+score,750,40);
   textSize(10);
-  text("Press Space to get a second Chance to Play!!",650 ,350);
+  text("Press Space to get a second Chance !",650 ,350);
   ground.display();
   stand1.display();
   stand2.display();
